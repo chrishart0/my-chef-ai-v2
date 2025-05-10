a712289524101eda2799e65c2d723c6916f122ea
chrishart0 <64378248+chrishart0@users.noreply.github.com>
2025-05-10 19:25:05 -0400

    Add search

--- src/agent/graph.py
+++ src/agent/graph.py
@@ -5,49 +5,73 @@ Returns a predefined response. Replace logic and configuration as needed.
 
 from __future__ import annotations
 
-from typing import TypedDict
-
 from langchain_core.runnables import RunnableConfig
 from langgraph.graph import StateGraph
-from langgraph.graph import StateGraph, START
+from langgraph.graph import StateGraph, START, END
 from langchain.chat_models import init_chat_model
 from langchain_tavily import TavilySearch
+from langgraph.prebuilt import ToolNode, tools_condition
+
 from agent.state import State
-
-class Configuration(TypedDict):
-    """Configurable parameters for the agent.
-
-    Set these when creating assistants OR when invoking the graph.
-    See: https://langchain-ai.github.io/langgraph/cloud/how-tos/configuration_cloud/
-    """
-    llm: str
-
-
-
-    
+from agent.configuration import Configuration
 
 graph_builder = StateGraph(State)
 
 
-llm = init_chat_model("openai:gpt-4.1")
+tool = TavilySearch(max_results=2)
+tools = [tool]
 
 def chatbot(state: State, config: RunnableConfig):
     llm_name = config["configurable"].get("llm", "openai:gpt-4.1")
     llm = init_chat_model(llm_name)
-    response = llm.invoke(state["messages"])
+    llm_with_tools = llm.bind_tools(tools)
+    response = llm_with_tools.invoke(state["messages"])
     return {"messages": state["messages"] + [response]}
 
+#################
+### Tool Node ###
+
+tool_node = ToolNode(tools=[tool])
+
+def route_tools(
+    state: State,
+):
+    """
+    Use in the conditional_edge to route to the ToolNode if the last message
+    has tool calls. Otherwise, route to the end.
+    """
+    if isinstance(state, list):
+        ai_message = state[-1]
+    elif messages := state.get("messages", []):
+        ai_message = messages[-1]
+    else:
+        raise ValueError(f"No messages found in input state to tool_edge: {state}")
+    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
+        return "tools"
+    return END
+
+
+#########################
 
 # The first argument is the unique node name
 # The second argument is the function or object that will be called whenever
 # the node is used.
 graph_builder.add_node("chatbot", chatbot)
-graph_builder.add_edge(START, "chatbot")
+graph_builder.add_node("tools", tool_node)
 
+graph_builder.add_edge(START, "chatbot")
+# The `tools_condition` function returns "tools" if the chatbot asks to use a tool, and "END" if
+# it is fine directly responding. This conditional routing defines the main agent loop.
+graph_builder.add_conditional_edges(
+    "chatbot",
+    tools_condition,
+)
+
+graph_builder.add_edge("tools", "chatbot")
 graph = graph_builder.compile()
 
 if __name__ == "__main__":
-    config = {"configurable": {"llm": "openai:gpt-4.1-mini"}}
+    config = Configuration(llm="openai:gpt-4.1-mini")
     messages = []
 
     def stream_graph_updates(messages, user_input: str, config=None):
