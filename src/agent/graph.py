f4f27e495f8f12f92053d586607e290709606081
chrishart0 <64378248+chrishart0@users.noreply.github.com>
2025-05-10 19:07:24 -0400

    Slight cleanup

--- src/agent/graph.py
+++ src/agent/graph.py
@@ -5,16 +5,14 @@ Returns a predefined response. Replace logic and configuration as needed.
 
 from __future__ import annotations
 
-from typing import TypedDict, Annotated, Sequence
+from typing import TypedDict
 
 from langchain_core.runnables import RunnableConfig
 from langgraph.graph import StateGraph
 from langgraph.graph import StateGraph, START
-from langgraph.graph.message import add_messages
 from langchain.chat_models import init_chat_model
-from langchain_core.messages import BaseMessage
 from langchain_tavily import TavilySearch
-
+from agent.state import State
 
 class Configuration(TypedDict):
     """Configurable parameters for the agent.
@@ -25,11 +23,7 @@ class Configuration(TypedDict):
     llm: str
 
 
-class State(TypedDict):
-    # Messages have the type "list". The `add_messages` function
-    # in the annotation defines how this state key should be updated
-    # (in this case, it appends messages to the list, rather than overwriting them)
-    messages: Annotated[Sequence[BaseMessage], add_messages]
+
     
 
 graph_builder = StateGraph(State)
